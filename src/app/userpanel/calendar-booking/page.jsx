2543620c9b23022ee11eef6dac6a9c7b3c68090c
Resource-02 <resource2.code4bharat@gmail.com>
2025-07-16 10:50:32 +0530

    update page.jsx

--- src/app/userpanel/calendar-booking/page.jsx
+++ src/app/userpanel/calendar-booking/page.jsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useState, useEffect } from 'react';
 import CalendarTimeSelection from '@/components/UserPanel/ExpertAboutMe/CalendarTimeSelection';
 import { startOfToday, addMonths, eachDayOfInterval, format } from 'date-fns';
