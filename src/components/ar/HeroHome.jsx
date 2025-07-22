e85a7fcbda31ee98919c2395c564659f5d588efa
Resource-04 <resource4.code4bharat@gmail.com>
2025-07-22 11:55:27 +0530

    update HeroHome.jsx

--- src/components/ar/HeroHome.jsx
+++ src/components/ar/HeroHome.jsx
@@ -12,7 +12,7 @@ function HeroHome() {
       >
         {/* Image on Left - Mobile Optimized */}
         <div className="w-full md:w-[50%] relative h-[60vh] md:h-screen overflow-hidden flex items-center justify-center mr-0 md:mr-10 mb-0 md:mb-0 mt-4 md:mt-0  md:px-0">
-          <div className="relative w-full h-full max-h-[60vh] md:max-h-none mx-2 md:mx-0">
+          <div className="relative md:-ml-32 w-full h-full max-h-[60vh] md:max-h-none mx-2 md:mx-0">
             <Image
               src="/HomeImg/homeHero.webp"
               alt="هيبة فراش"
@@ -25,30 +25,29 @@ function HeroHome() {
           </div>
         </div>
 
-{/* Arabic Text on Right - Mobile Center, Desktop Right */}
+       {/* Arabic Text on Right - Mobile Center, Desktop Right */}
         <div
-          className="w-full md:w-[50%] flex flex-col  justify-center items-center md:items-end mt-8 md:mt-0 p-4 md:p-12 z-10 md:ml-60"
+          className="w-full md:w-[50%] flex flex-col justify-center items-center md:items-end mt-8 md:mt-0 p-4 md:p-12 z-10"
           dir="rtl"
         >
-          <h3 className="text-5xl sm:text-2xl md:text-7xl font-rpt-Bold text-black text-center md:text-right leading-normal uppercase md:mb-7 md:-ml-[2.9rem] font-black w-full" 
+          <h3 className="text-5xl sm:text-2xl md:text-7xl font-rpt-Bold text-black text-center md:text-right leading-normal uppercase md:mb-7 font-black w-full md:max-w-none md:pr-0" 
               style={{ fontWeight: '900' }}>
             لمسة تدوم من
           </h3>
 
-  <h1
-  className="text-5xl sm:text-3xl md:text-7xl font-rpt-Bold text-black text-center md:text-right md:px-10 leading-normal uppercase mb-4 font-black w-full"
-  style={{
-    fontFamily: "rpt-Bold, sans-serif",
-    display: "inline-block",
-    fontWeight: "900",
-    letterSpacing: "0.2em", // Adjust the spacing between the letters
-  }}
->
-  هـــــبــــة فـــــراش
-</h1>
+          <h1
+            className="text-5xl sm:text-3xl md:text-7xl font-rpt-Bold text-black text-center md:text-right leading-normal uppercase mb-4 font-black w-full md:max-w-none"
+            style={{
+              fontFamily: "rpt-Bold, sans-serif",
+              display: "inline-block",
+              fontWeight: "900",
+              letterSpacing: "0.2em", // Adjust the spacing between the letters
+            }}
+          >
+            هـــــبــــة فـــــراش
+          </h1>
 
-
-          <p className="text-xl sm:text-lg md:text-3xl font-rpt-Bold text-black uppercase leading-normal text-center md:text-right mb-6 md:mt-10 md:mb-20 max-w-lg md:-ml-[4.1rem] px-2 md:px-0 font-black w-full"
+          <p className="text-xl sm:text-lg md:text-3xl font-rpt-Bold text-black uppercase leading-normal text-center md:text-right mb-6 md:mt-10 md:mb-20 max-w-lg md:max-w-none px-2 md:px-0 font-black w-full"
              style={{ fontWeight: '900' }}>
             مصممة أزياء سعودية حاصلة على عدة شهادات وجوائز في مجال
             الأزياء، ومدربة معتمدة قدّمت مشاركات بارزة في محافل محلية ودولية،
@@ -60,7 +59,7 @@ function HeroHome() {
             تقدّم جلسات استشارية في الذوق، والأناقة، وبناء الهوية الشخصية
           </h5> */}
 
-          <div className="w-full flex justify-center md:justify-end md:pr-14 md:ml-7 mt-4 -mb-6 md:mt-0">
+          <div className="w-full flex md:ml-24 justify-center md:justify-end mt-4 -mb-6 md:mt-0">
             <Link href="/userpanel/booksession">
               <button className="bg-black text-white text-xl md:text-base py-4 px-12 md:py-4 md:px-20 rounded-2xl uppercase font-rpt-Bold hover:bg-gray-800 transition font-black"
                       style={{ fontWeight: '900' }}>
@@ -74,7 +73,7 @@ function HeroHome() {
       {/* ✅ Supported By Section */}
       <section className="bg-white py-12 md:py-20">
         <div className="text-center" dir="rtl">
-          <h2 className="text-lg md:text-3xl font-rpt-Bold uppercase tracking-wide text-gray-800 "style={{ fontWeight: '900' }}>
+          <h2 className="text-lg md:text-3xl font-rpt-Bold uppercase tracking-wide text-gray-800 " style={{ fontWeight: '900' }}>
             بدعم من
           </h2>
           <p className="mt-2 text-lg md:text-xl font-rpt-Bold text-gray-500" style={{ fontWeight: '900' }}>
